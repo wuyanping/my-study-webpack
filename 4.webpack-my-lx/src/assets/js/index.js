07b91a1ed17707c99c9e730f11719c971743c5f4
wuyanping <1063218918@qq.com>
2018-02-26 12:50:42 +0800

    [mod] add 4

---
+++ 4.webpack-my-lx/src/assets/js/index.js
@@ -0,0 +1,3 @@
+// require('../sass/style.scss')
+module.exports = "index.js"
+// document.wirte(require('./other.js'))
---
+++ 4.webpack-my-lx/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+// "start": "webpack --env.prod"
+// const config = env => {
+// 	console.log(env)
+// 	return {
+// 		//入口
+// 		// entry: './src/assets/js/index.js',
+// 		entry: {
+// 			bundle: './src/assets/js/index.js'
+// 		},
+// 		output: {
+// 			path: path.resolve(__dirname, 'build'),
+// 			filename: 'js/bundle.js',
+// 			publicPath: "/assets/"
+// 		}
+// 	}
+// }
+console.log(process.env.NODE_ENV)
+const config = {
+	//入口
+	// entry: './src/assets/js/index.js',
+	entry: {
+		bundle: process.env.NODE_ENV === 'dev' ? './src/assets/js/index.js' : './src/assets/js/other.js'
+	},
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'js/bundle.js',
+		publicPath: "/assets/"
+	}
+}
+module.exports = config
